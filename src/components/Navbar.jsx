0e073ec9be73a19a366ed878436e09be941a2479
Niran Amalore <129084838+NiranAmalore@users.noreply.github.com>
2024-10-28 00:10:22 +0530

    Updated Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Navbar(){
     return (
         <nav className="navbar">
-            <img className="navbar--logo" src="./src/images/Globe.png"/>
+            <img className="navbar--logo" src="../images/Globe.png"/>
             <p className="navbar--title">my travel journal.</p>
         </nav>
     )
