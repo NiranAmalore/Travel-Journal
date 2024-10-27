0d395ce12261bb9fa3ce153228cf514154d8c190
Niran Amalore <129084838+NiranAmalore@users.noreply.github.com>
2024-10-28 00:15:19 +0530

    Updated Navbar.jsx
    Updated by importing img as a variable

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from "react"
+import globeImg from '../images/Globe.png'
 
 export default function Navbar(){
     return (
         <nav className="navbar">
-            <img className="navbar--logo" src="../images/Globe.png"/>
+            <img className="navbar--logo" src={globeImg}/>
             <p className="navbar--title">my travel journal.</p>
         </nav>
     )
